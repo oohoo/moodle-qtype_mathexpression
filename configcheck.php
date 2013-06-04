debb7ca7650fa33b3080bea368992d35618edfba
Raymond Wainman <raywainman@gmail.com>
2013-06-04 13:59:27 -0600

    missing copyright headers

--- configcheck.php
+++ configcheck.php
@@ -1,5 +1,19 @@
 <?php
 
+/**
+ * *************************************************************************
+ * *                            MathExpression                            **
+ * *************************************************************************
+ * @package     question                                                  **
+ * @subpackage  mathexpression                                            **
+ * @name        MathExpression                                            **
+ * @copyright   oohoo.biz                                                 **
+ * @link        http://oohoo.biz                                          **
+ * @author      Raymond Wainman (wainman@ualberta.ca)                     **
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later  **
+ * *************************************************************************
+ * ************************************************************************ */
+
 require('../../../config.php');
 
 $PAGE->set_pagelayout('admin');
--- settings.php
+++ settings.php
@@ -1,5 +1,19 @@
 <?php
 
+/**
+ * *************************************************************************
+ * *                            MathExpression                            **
+ * *************************************************************************
+ * @package     question                                                  **
+ * @subpackage  mathexpression                                            **
+ * @name        MathExpression                                            **
+ * @copyright   oohoo.biz                                                 **
+ * @link        http://oohoo.biz                                          **
+ * @author      Raymond Wainman (wainman@ualberta.ca)                     **
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later  **
+ * *************************************************************************
+ * ************************************************************************ */
+
 $settings->add(new admin_setting_configtext('qtype_mathexpression_sageserver',
     get_string('sageserver','qtype_mathexpression'),
     get_string('sageserver_help', 'qtype_mathexpression'), ''));
