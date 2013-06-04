30a9b2ebf61d0008dd965ada8e8cda9d988f4c4a
Raymond Wainman <raywainman@gmail.com>
2013-06-04 11:34:13 -0600

    default for exclude variable is empty array

--- server/server.py
+++ server/server.py
@@ -46,7 +46,7 @@ class sage_evaluate:
 
 class sage_full:
     def GET(self):
-        args = web.input(expr1='', expr2='', vars='', exclude='')
+        args = web.input(expr1='', expr2='', vars='', exclude='[]')
         result = sageserver.full_compare(args.expr1, args.expr2, args.vars, json.loads(args.exclude))
         return json.dumps(result)
 
